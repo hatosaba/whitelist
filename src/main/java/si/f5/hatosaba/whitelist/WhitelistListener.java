5d984ac13cc06b7660ece8f45e97c149e0a955d2
oieii <lpondboy@gmail.com>
2021-12-20 14:10:17 +0900

    fix:プレイヤーが参加したときにメッセージを送る

--- src/main/java/si/f5/hatosaba/whitelist/WhitelistListener.java
+++ src/main/java/si/f5/hatosaba/whitelist/WhitelistListener.java
@@ -4,34 +4,37 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 
+import static org.bukkit.ChatColor.*;
 import static org.bukkit.ChatColor.YELLOW;
-import static org.bukkit.ChatColor.RED;
-import static org.bukkit.ChatColor.RESET;
 
 public class WhitelistListener implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onLogin(PlayerLoginEvent e) {
         Player player = e.getPlayer();
-        boolean isWhitelist = Whitelist.plugin().isWhitelist();
 
-        if (isWhitelist) {
+        if (Whitelist.plugin().isWhitelist()) {
             if (player.hasPermission("perm.whitelist")) return;
             e.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, "ホワイトリストに登録されていません");
         }
+    }
 
-        if(!player.hasPermission("hatosaba.admin")) return;
-
-        if (isWhitelist) {
-            player.sendMessage("§e----------------------\n" +
-                    " ホワイトリストは" + RED + "有効" + RESET + "です" +
-                    "----------------------");
-        }else {
-            player.sendMessage(YELLOW + "----------------------\n" +
-                    " ホワイトリストは§c無効§rです\n" +
-                    YELLOW +"----------------------");
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        if(player.hasPermission("hatosaba.admin")) {
+            if (Whitelist.plugin().isWhitelist()) {
+                player.sendMessage("§e----------------------\n" +
+                        " ホワイトリストは" + RED + "有効" + RESET + "です" +
+                        "----------------------");
+            }else {
+                player.sendMessage(YELLOW + "----------------------\n" +
+                        " ホワイトリストは§c無効§rです\n" +
+                        YELLOW +"----------------------");
+            }
         }
     }
 }
